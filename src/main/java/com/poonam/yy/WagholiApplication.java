bfb7eeab030d54db4da08ebed6246ff8d9666e97
Poonamsule <poonamsonwalkar@gmail.com>
2020-08-03 01:39:22 +0530

    tuuuuuuuuuu

---
+++ src/main/java/com/poonam/yy/WagholiApplication.java
@@ -0,0 +1,13 @@
+package com.poonam.yy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WagholiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WagholiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poonam/yy/WagholiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poonam.yy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WagholiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
